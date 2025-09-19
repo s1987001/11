f5fe83d9d2396163f6fbd6a42abcafd327a51712
s1987001 <76094319+s1987001@users.noreply.github.com>
2025-09-19 09:34:21 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://www.yirens.top:21114".to_owned()
+    "http://5.yirens.top:21114".to_owned()
 }
 
 #[inline]
